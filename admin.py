06f0b882a5d527998d56b57d92d81a0316ddc6c1
Omkar Joshi <joshi112omkar@gmail.com>
2026-08-27 12:16:08 +0530

    Delete admin.py

--- admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Key,Certificate,Transaction,Incident,AuditLog
-for model in [Key,Certificate,Transaction,Incident,AuditLog]:
-    admin.site.register(model)
