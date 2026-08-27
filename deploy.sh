8e08fe64c3348a77e9e63743008c2a324d5ac7ff
Omkar Joshi <joshi112omkar@gmail.com>
2026-08-27 12:15:47 +0530

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-docker compose up -d --build
-docker compose exec web python manage.py migrate --noinput
-docker compose exec web python manage.py seed_demo
-docker compose ps
